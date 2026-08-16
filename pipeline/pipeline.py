e82d4ed499c75d95d9f3476de7dc099b54d44bee
Serge <billcipher806@gmail.com>
2026-08-16 22:55:03 +0000

    create docker compose for postgres db, postgres web client and pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,18 +1,89 @@
-import sys
+from dotenv import load_dotenv
+import os
 import pandas as pd
+from sqlalchemy import create_engine
 
-if len(sys.argv) != 2:
-    print("invalid number of arguments passed")
-    sys.exit(1)
+load_dotenv()
 
-day = int(sys.argv[1])
-print(f"Running pipeline for day: {day}")
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
 
-df = pd.DataFrame(data={
-    "A": [1, 2],
-    "B": [3, 4]
-})
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
 
-print(df.head())
+def ingest(
+    url: str,
+    engine,
+    target_table: str,
+    chunk_size: int = 100_000
+):
+    df_iter = pd.read_csv(
+        url,
+        iterator=True,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        chunksize=chunk_size
+    )
 
-df.to_parquet(f"output_day_{day}.parquet")
+    first = True
+
+    for chunk in df_iter:
+        if first:
+            # Create a table
+            chunk.head(n=0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            print("Table has been created")
+
+            first = False
+
+        chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
+        print(f"Inserted chunk: {len(chunk)} rows")
+
+
+def main():
+    pg_user = os.environ.get("POSTGRES_USER")
+    pg_pass = os.environ.get("POSTGRES_PASSWORD")
+    pg_db = os.environ.get("POSTGRES_DB")
+    pg_host = os.environ.get("POSTGRES_HOST")
+    pg_port = os.environ.get("POSTGRES_PORT")
+    url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+    target_table = 'yellow_taxi_data'
+    chunk_size = 100_000
+
+    engine = create_engine(f"postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
+
+    ingest(
+        url=url,
+        engine=engine,
+        target_table=target_table,
+        chunk_size=chunk_size
+    )
+
+
+if __name__ == "__main__":
+    main()
